ddb46b461429ff13f9e9b1131398246f48911c11
Mouhari Mouhamed <mmouhamed@healthemed.com>
2025-02-18 18:18:48 -0600

    attendance chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div>
+      <h1 className="flex p-10 items-center justify-center ">Homepage</h1>
 
-export default Homepage
+      <Link href="/admin">
+        <button className="justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Admin
+        </button>
+      </Link>
+    </div>
+  );
+};
+export default Homepage;
--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import {
   BarChart,
   Bar,
-  Rectangle,
   XAxis,
   YAxis,
   CartesianGrid,
@@ -54,23 +53,44 @@ const data = [
 const AttendanceChart = () => {
   return (
     <div className="bg-white rounded-lg w-full h-full p-4">
-      <div>
+      <div className="flex justify-between items-center">
         <h1 className="text-lg font-semibold">Attendance Chart</h1>
         <Image src="/moreDark.png" alt="" width={20} height={20} />
       </div>
       <ResponsiveContainer width="100%" height="90%">
         <BarChart width={500} height={300} data={data} barSize={20}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
+          <CartesianGrid
+            strokeDasharray="3 3"
+            vertical={false}
+            stroke="#dddddd"
+          />
+          <XAxis
+            dataKey="name"
+            axisLine={false}
+            tick={{ fill: "#d1d5db" }}
+            tickLine={false}
+          />
+          <YAxis axisLine={false} tick={{ fill: "#d1d5db" }} tickLine={false} />
+          <Tooltip
+            contentStyle={{ borderRadius: "10px", borderColor: "lightgray" }}
+          />
           <Legend
             align="left"
             verticalAlign="top"
             wrapperStyle={{ paddingTop: "20px", paddingBottom: "40px" }}
           />
-          <Bar dataKey="absent" fill="#FAE27C" legendType="circle" />
-          <Bar dataKey="present" fill="#CFC4FF" legendType="circle" />
+          <Bar
+            dataKey="absent"
+            fill="#FAE27C"
+            legendType="circle"
+            radius={[10, 10, 0, 0]}
+          />
+          <Bar
+            dataKey="present"
+            fill="#CFC4FF"
+            legendType="circle"
+            radius={[10, 10, 0, 0]}
+          />
         </BarChart>
       </ResponsiveContainer>
     </div>
